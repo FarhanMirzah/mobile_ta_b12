7266deb238228be6105800db726f327711adad2d
ThomasAkram <91660351+ThomasAkram@users.noreply.github.com>
2022-09-24 16:16:19 +0700

    24 sept 2022

--- app/src/main/java/com/example/mobile_ta_b12/LoginActivity.java
+++ app/src/main/java/com/example/mobile_ta_b12/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.mobile_ta_b12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void login(View view) {
+        Intent login1 = new Intent(LoginActivity. this,ListMahasiswaActivity.class);
+        startActivity(login1);
     }
 }
