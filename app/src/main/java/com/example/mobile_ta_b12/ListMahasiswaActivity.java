0a5f91ac63d8ba44db0f61baa4d294844540abf9
ThomasAkram <91660351+ThomasAkram@users.noreply.github.com>
2022-09-25 12:57:16 +0700

    25 sept 2022

--- app/src/main/java/com/example/mobile_ta_b12/ListMahasiswaActivity.java
+++ app/src/main/java/com/example/mobile_ta_b12/ListMahasiswaActivity.java
@@ -2,7 +2,9 @@ package com.example.mobile_ta_b12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListMahasiswaActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ListMahasiswaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_mahasiswa);
+    }
+
+    public void profile(View view) {
+        Intent lihatprofile = new Intent(ListMahasiswaActivity.this, ProfileActivity.class);
+        startActivity(lihatprofile);
     }
 }
