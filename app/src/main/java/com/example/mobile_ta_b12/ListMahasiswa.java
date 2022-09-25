e7ac7a40705450a7b97575afc6821f01cc84d2f3
ThomasAkram <91660351+ThomasAkram@users.noreply.github.com>
2022-09-25 14:21:58 +0700

    25 sept 2022

---
+++ app/src/main/java/com/example/mobile_ta_b12/ListMahasiswa.java
@@ -0,0 +1,22 @@
+package com.example.mobile_ta_b12;
+
+public class ListMahasiswa {
+    private String nama_mahasiswa;
+    private String nim_mahasiswa;
+
+    public String getNama_mahasiswa() {
+        return nama_mahasiswa;
+    }
+
+    public String getNim_mahasiswa() {
+        return nim_mahasiswa;
+    }
+
+    public void setNama_mahasiswa(String nama_mahasiswa) {
+        this.nama_mahasiswa = nama_mahasiswa;
+    }
+
+    public void setNim_peserta(String nim_mahasiswa) {
+        this.nim_mahasiswa = nim_mahasiswa;
+    }
+}
--- app/src/main/java/com/example/mobile_ta_b12/ListMahasiswaActivity.java
+++ app/src/main/java/com/example/mobile_ta_b12/ListMahasiswaActivity.java
@@ -1,21 +1,64 @@
 package com.example.mobile_ta_b12;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListMahasiswaActivity extends AppCompatActivity {
+    private RecyclerView listmhs;
+    private ArrayList<ListMahasiswa> list = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_mahasiswa);
+
+        listmhs = findViewById(R.id.recycleview_listmahasiswa);
+        listmhs.setHasFixedSize(true);
+
+        list.addAll(getListMahasiswaa());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        listmhs.setLayoutManager(new LinearLayoutManager(this));
+        ListMahasiswaAdapter listHeroAdapter = new ListMahasiswaAdapter(list);
+        listmhs.setAdapter(listHeroAdapter);
+    }
+
+    public ArrayList<ListMahasiswa> getListMahasiswaa() {
+        String[] nama_mahasiswa = getResources().getStringArray(R.array.nama_mahasiswa);
+        String[] nim_mahasiswa = getResources().getStringArray(R.array.nim_mahasiswa);
+        ArrayList<ListMahasiswa> listMahasiswab = new ArrayList<>();
+        for (int i = 0; i < nama_mahasiswa.length; i++) {
+            ListMahasiswa listMahasiswa = new ListMahasiswa();
+            listMahasiswa.setNama_mahasiswa(nama_mahasiswa[i]);
+            listMahasiswa.setNim_peserta(nim_mahasiswa[i]);
+            listMahasiswab.add(listMahasiswa);
+        }
+        return listMahasiswab;
     }
 
     public void profile(View view) {
-        Intent lihatprofile = new Intent(ListMahasiswaActivity.this, ProfileActivity.class);
-        startActivity(lihatprofile);
+        Intent intent = new Intent(ListMahasiswaActivity.this, ProfileActivity.class);
+        startActivity(intent);
+
+//    public void tambahLB(View view) {
+//        Intent intent = new Intent(LogbookActivity.this,AddlogbookActivity.class);
+//        startActivity(intent);
+//    }
     }
 }
