72a28533cc85b5bcf1ee1b9f380669f049bd4b3e
FarhanMirzah <farhan.anandamirzah@gmail.com>
2022-09-26 17:41:30 +0700

    Layout Input Nilai TA

---
+++ app/src/main/java/com/example/mobile_ta_b12/InputNilaiTugasAkhirActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobile_ta_b12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputNilaiTugasAkhirActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_nilai_tugas_akhir);
+    }
+}
