3818ff8daf7bd6e92637ea8a3576021a20c537fc
FarhanMirzah <farhan.anandamirzah@gmail.com>
2022-10-03 15:30:07 +0700

    RecyclerView List Logbook (belum interaktif)

---
+++ app/src/main/java/com/example/mobile_ta_b12/ListLogbookActivity.java
@@ -0,0 +1,46 @@
+package com.example.mobile_ta_b12;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.mobile_ta_b12.adapter.ListLogbookAdapter;
+import com.example.mobile_ta_b12.models.ListLogbook;
+
+import java.util.ArrayList;
+
+public class ListLogbookActivity extends AppCompatActivity {
+
+    private RecyclerView rvLogbook;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_logbook);
+
+        rvLogbook = findViewById(R.id.rv_logbook);
+
+        ListLogbookAdapter adapter = new ListLogbookAdapter(getListLogbook());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        rvLogbook.setLayoutManager(layoutManager);
+        rvLogbook.setAdapter(adapter);
+    }
+
+    public ArrayList<ListLogbook> getListLogbook(){
+        ArrayList<ListLogbook> listLogbook = new ArrayList<>();
+        listLogbook.add(new ListLogbook(
+                "Jumat, 6 Mei 2022",
+                "Menentukan judul",
+                1
+        ));
+        listLogbook.add(new ListLogbook(
+                "Sabtu, 7 Mei 2022",
+                "Membuat latar belakang",
+                0
+        ));
+        return listLogbook;
+    }
+}
---
+++ app/src/main/java/com/example/mobile_ta_b12/adapter/ListLogbookAdapter.java
@@ -0,0 +1,64 @@
+package com.example.mobile_ta_b12.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mobile_ta_b12.R;
+import com.example.mobile_ta_b12.models.ListLogbook;
+
+import java.util.ArrayList;
+
+public class ListLogbookAdapter extends RecyclerView.Adapter<ListLogbookAdapter.ListLogbookViewHolder>{
+
+    ArrayList<ListLogbook> listLogbook = new ArrayList<>();
+
+    public ListLogbookAdapter(ArrayList<ListLogbook> listLogbook) {
+        this.listLogbook = listLogbook;
+    }
+
+    public void setListLogbook(ArrayList<ListLogbook> listLogbook) {
+        this.listLogbook = listLogbook;
+    }
+
+    @NonNull
+    @Override
+    public ListLogbookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_logbook, parent, false);
+        return new ListLogbookViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListLogbookViewHolder holder, int position) {
+        ListLogbook logbook = listLogbook.get(position);
+        holder.imageStatus.setImageResource(R.drawable.ic_check_circle_outline_green);
+        holder.textHariTanggal.setText(logbook.getHariTanggal());
+        holder.textKegiatan.setText(logbook.getKegiatan());
+        holder.textStatus.setText(Integer.toString(logbook.getStatus()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listLogbook.size();
+    }
+
+    public class ListLogbookViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView imageStatus;
+        TextView textHariTanggal, textKegiatan, textStatus;
+
+        public ListLogbookViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageStatus = itemView.findViewById(R.id.imageStatus);
+            textHariTanggal = itemView.findViewById(R.id.textHariTanggal);
+            textKegiatan = itemView.findViewById(R.id.textKegiatan);
+            textStatus = itemView.findViewById(R.id.textStatus);
+        }
+    }
+}
