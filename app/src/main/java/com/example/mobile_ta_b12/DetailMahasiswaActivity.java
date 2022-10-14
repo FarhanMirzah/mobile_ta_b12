7392d3b5444c6f5db893873f86dd8da06a612310
ThomasAkram <91660351+ThomasAkram@users.noreply.github.com>
2022-10-14 19:26:45 +0700

    detail mahasiswa dan pembatalan ta

---
+++ app/src/main/java/com/example/mobile_ta_b12/DetailMahasiswaActivity.java
@@ -0,0 +1,20 @@
+package com.example.mobile_ta_b12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class DetailMahasiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_mahasiswa);
+    }
+    public void tombolbackDetailMahasiswa(View view) {
+        Intent back = new Intent(this, DetailTugasAkhirActivity.class);
+        startActivity(back);
+    }
+}
