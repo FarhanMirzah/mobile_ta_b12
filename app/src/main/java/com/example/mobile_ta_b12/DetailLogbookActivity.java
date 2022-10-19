8aae3dc5b1b9fad4c04fa2462a0330bc0f50d687
FarhanMirzah <farhan.anandamirzah@gmail.com>
2022-10-19 13:58:04 +0700

    Detail Logbook membaca kegiatan yang di klik di List logbook

--- app/src/main/java/com/example/mobile_ta_b12/DetailLogbookActivity.java
+++ app/src/main/java/com/example/mobile_ta_b12/DetailLogbookActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.mobile_ta_b12.adapter.LogbookAdapter;
@@ -19,7 +20,12 @@ public class DetailLogbookActivity extends AppCompatActivity {
     private RecyclerView rvLogbook;
 
     String hariTanggal;
+    String kegiatan;
+    int status;
     TextView textDetailHariTanggal;
+    TextView textDetailKegiatan;
+    ImageView imageStatusLogbook;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +35,17 @@ public class DetailLogbookActivity extends AppCompatActivity {
         Intent detailIntent = getIntent();
         if(detailIntent != null){
             hariTanggal = detailIntent.getStringExtra("HARI_TANGGAL");
+            kegiatan = detailIntent.getStringExtra("KEGIATAN");
+//            status = detailIntent.getIntExtra("STATUS", 1);
         }
         textDetailHariTanggal = findViewById(R.id.textDetailHariTanggal);
         textDetailHariTanggal.setText(hariTanggal);
+
+        textDetailKegiatan = findViewById(R.id.textDetailKegiatan);
+        textDetailKegiatan.setText(kegiatan);
+
+//        imageStatusLogbook = findViewById(R.id.imageStatusLogbook);
+//        imageStatusLogbook.setImageResource(status);
     }
 
     public void buttonBackListLogbook(View view) {
