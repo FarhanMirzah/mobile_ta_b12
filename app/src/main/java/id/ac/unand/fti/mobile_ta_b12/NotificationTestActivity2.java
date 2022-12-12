8abb8691b6d0f53c30b324b9f7211280eff6a927
ThomasAkram <91660351+ThomasAkram@users.noreply.github.com>
2022-12-12 23:11:26 +0700

    tes notif

---
+++ app/src/main/java/id/ac/unand/fti/mobile_ta_b12/NotificationTestActivity2.java
@@ -0,0 +1,100 @@
+package id.ac.unand.fti.mobile_ta_b12;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class NotificationTestActivity2 extends AppCompatActivity {
+
+    private static final String TAG = "Notification-Debug";
+    private static final String CHANNEL_ID = "test_channel" ;
+    private Button buttonShow;
+    private NotificationManagerCompat notificationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_test);
+
+        // 1. Ambil notification manager
+        notificationManager = NotificationManagerCompat.from(this);
+
+        // 2b. Buat channel notifikasi
+        createNotificationChannel();
+
+        buttonShow = findViewById(R.id.buttonShow);
+        buttonShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent resultIntent = new Intent(NotificationTestActivity2.this, ListLogbookActivity.class);
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(NotificationTestActivity2.this);
+                stackBuilder.addNextIntentWithParentStack(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(0,
+                                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+                // 3. Buat builder untuk membuat notifikasi
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(NotificationTestActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_baseline_warning_24)
+                        .setContentTitle("Notifikasi Logbook")
+                        .setContentText("Mahasiswa mengupdate Logbook")
+//                        .setContentIntent(resultPendingIntent)
+                        .addAction(R.drawable.ic_baseline_warning_24, "Lihat", resultPendingIntent)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                // 4. Buat objek notifikasi
+                Notification notification = builder.build();
+
+                // 5. Tampilkan notifikasi
+                notificationManager.notify(101, notification);
+            }
+        });
+
+        // Kode token Firebase
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        Log.d(TAG, token);
+                        Toast.makeText(NotificationTestActivity2.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    // 2a. Buat channel
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Channel Contoh", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Ini hanya channel untuk contoh notifikasi");
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
