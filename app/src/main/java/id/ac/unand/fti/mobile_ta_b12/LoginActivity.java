2c306199ac3b85f33e9ce693a1852ada4514679b
FarhanMirzah <farhan.anandamirzah@gmail.com>
2022-12-17 21:08:35 +0700

    Login Retrofit (PTB Lanjutan Retrofit pt 1, 31:30)

--- app/src/main/java/id/ac/unand/fti/mobile_ta_b12/LoginActivity.java
+++ app/src/main/java/id/ac/unand/fti/mobile_ta_b12/LoginActivity.java
@@ -2,8 +2,11 @@ package id.ac.unand.fti.mobile_ta_b12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,12 +45,6 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(view);
 
         cekLogin();
-
-
-
-        // Kode lama (findViewById)
-//        editUsername = findViewById(R.id.editUsername);
-//        editPassword = findViewById(R.id.editPassword);
     }
 
     private void cekLogin() {
@@ -55,13 +52,12 @@ public class LoginActivity extends AppCompatActivity {
             editPassword = findViewById(R.id.editPassword);
             button = findViewById(R.id.button);
 
-            String username= binding.editUsername.getText().toString();
-            String password = binding.editPassword.getText().toString();
-
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     String API_BASE_URL = "http://ptb-api.husnilkamil.my.id/";
+                    String username= binding.editUsername.getText().toString();
+                    String password = binding.editPassword.getText().toString();
 
                     Retrofit retrofit = new Retrofit.Builder()
                             .baseUrl(API_BASE_URL)
@@ -77,10 +73,14 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
                             LoginResponse loginResponse = response.body();
+                            Log.d("LoginAct-Debug", response.toString());
                             if(loginResponse != null){
                                 Toast.makeText(LoginActivity.this, "Berhasil Login", Toast.LENGTH_SHORT).show();
-                                Intent homeIntent = new Intent(LoginActivity.this,ListMahasiswaActivity.class);
-                                startActivity(homeIntent);
+
+                                Intent mainIntent = new Intent(LoginActivity.this,ListMahasiswaActivity.class);
+                                mainIntent.putExtra("USERNAME", username);
+                                mainIntent.putExtra("IS_LOGGED_IN", true);
+                                startActivity(mainIntent);
                             }
                             else{
                                 Toast.makeText(LoginActivity.this, "Username Atau Pasword Salah", Toast.LENGTH_SHORT).show();
@@ -89,8 +89,8 @@ public class LoginActivity extends AppCompatActivity {
 
                         @Override
                         public void onFailure(Call<LoginResponse> call, Throwable t) {
+                            Log.d("LoginAct-Debug", t.toString());
                             Toast.makeText(LoginActivity.this, "Gagal menghubungi server", Toast.LENGTH_SHORT).show();
-
                         }
                     });
 
@@ -98,32 +98,9 @@ public class LoginActivity extends AppCompatActivity {
             });
     }
 
-
-
-
-//    public void onButtonLoginClick(View view) {
-//        // Kode lama (findViewById)
-////        String username = editUsername.getText().toString();
-////        String password = editPassword.getText().toString();
-//
-//        // Kode baru (View Binding)
-//        String username = binding.editUsername.getText().toString();
-//        String password = binding.editPassword.getText().toString();
-//
-//        if(username.equals("Husnil Kamil") && password.equals("123")){
-//            Intent mainIntent = new Intent(LoginActivity. this,ListMahasiswaActivity.class);
-//            mainIntent.putExtra("USERNAME", username);
-//            mainIntent.putExtra("IS_LOGGED_IN", true);
-//            startActivity(mainIntent);
-//            finish();
-//        }else{
-//            Toast.makeText(this, "Username atau Password salah", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//    @Override
-//    public void onBackPressed()
-//    {
-//        moveTaskToBack(true);
-//    }
+    @Override
+    public void onBackPressed()
+    {
+        moveTaskToBack(true);
+    }
 }
